8b378af176bcc87254b2279ec8cc54f9b5fd7513
MinexAutomation <minex.automation@gmail.com>
2019-10-26 13:38:31 -0700

    Added remote deployment.

--- source/ΩMaintenance/Code/Program.cs
+++ source/ΩMaintenance/Code/Program.cs
@@ -9,15 +9,27 @@ namespace ΩMaintenance
     {
         static void Main(string[] args)
         {
-            Program.DeployScript();
+            //Program.LocalDeployScript();
+            Program.RemoteDeployScript();
         }
 
         /// <summary>
-        /// Deploys (copies compilation output directory binary and other files) to a file-system site specified by an R5T.Antium.Base.IDeploymentDestinationFileSystemSiteProvider.
+        /// Deploys (copies compilation output directory binary and other files) to a remote file-system site.
+        /// </summary>
+        private static void RemoteDeployScript()
+        {
+            var remoteDeploymentSecretsFileName = "R5T.Rome.HelloWorld-RemoteDeployment.json";
+            var entryPointProjectName = "R5T.Rome.HelloWorld";
+
+            RomeUtilities.DeployRemote(remoteDeploymentSecretsFileName, entryPointProjectName);
+        }
+
+        /// <summary>
+        /// Deploys (copies compilation output directory binary and other files) to a local file-system site specified by an R5T.Antium.Base.IDeploymentDestinationFileSystemSiteProvider.
         /// The implementation of which in R5T.Antium.Default uses an R5T.Suebia.Base.ISecretsFilePathProvider and an R5T.Antium.Default.IDeploymentDestinationSecretsFileNameProvider to get details of the deployment file-system site.
         /// The IDeploymentDestinationSecretsFileNameProvider implementation in R5T.Antium.Default uses a file-name constructor parameter, and is meant to be instantiated in this maintenance console project, and added to the service provider configured by this maintenance console project as a singleton.
         /// </summary>
-        private static void DeployScript()
+        private static void LocalDeployScript()
         {
             var localDeploymentSecretsFileName = "R5T.Rome.HelloWorld-LocalDeployment.json";
             var entryPointProjectName = "R5T.Rome.HelloWorld";
